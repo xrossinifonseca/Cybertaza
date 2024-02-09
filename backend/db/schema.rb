cc5a1ddaebfe92aa10a4f12af04ae6928aa0bcc3
Rossini fonseca <rossinidrum@outlook.com>
2024-02-09 14:53:05 -0300

    chore(backend)  add new migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_001854) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_021423) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
