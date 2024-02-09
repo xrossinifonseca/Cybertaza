ac770b27a7ffe29f7afccd1e93da0b8965882edb
Rossini fonseca <rossinidrum@outlook.com>
2024-02-09 14:52:15 -0300

    feat:(backend) add validations

--- backend/app/models/customer.rb
+++ backend/app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+
+  has_secure_password
+
+  validates :name, presence: { message: "O nome é obrigatório" }
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,message:"Email inválido" }
+  validates :password, presence: { message: "A senha é obrigatória" }, length: { minimum: 6, message: "A senha deve ter pelo menos 6 caracteres" }
 end
