61317df7ac78f7f30309e2126ee195efe1167b0c
Rossini fonseca <rossinidrum@outlook.com>
2024-02-09 14:54:03 -0300

    feat(backend): Introduce methods for token management

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
-class ApplicationController < ActionController::Base
+
+class ApplicationController < ActionController::API
+
+  private
+  def generate_token(payload)
+    JWT.encode(payload, ENV["JWT_KEY"], 'HS256')
+  end
+
+  private
+  def decode_token(token)
+    JWT.decode(token, ENV["JWT_KEY"], true, algorithm: 'HS256')[0]
+  rescue JWT::DecodeError
+    nil
+  end
+
+  protected
+  def create_token(id)
+    payload = {customer_id:id}
+    token = generate_token(payload)
+  end
+
 end
