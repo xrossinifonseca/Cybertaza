92c24954f7b120cd00f45fd81a0da254ba2886fe
Rossini fonseca <rossinidrum@outlook.com>
2024-02-22 12:55:16 -0300

    feat(backend): cria classe AdminController e adiciona metodo login

---
+++ backend/app/controllers/admin_controller.rb
@@ -0,0 +1,27 @@
+
+class AdminController < ApplicationController
+
+
+  def login
+    user = User.find_by(email:session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      token = create_admin_token(user.id)
+      set_cookie(token)
+      return render json: { message: "Login bem-sucedido", user: user.as_json(except: :password_digest) }, status: :ok
+    end
+
+    render json: { error: "Credenciais inválidas" }, status: :unauthorized
+  end
+
+  def logout
+    cookies.delete(:auth_token)
+    render json: { message: "Logout bem-sucedido" }, status: :ok
+  end
+
+  private
+  def session_params
+    params.require(:admin).permit(:email, :password)
+  end
+
+
+end
