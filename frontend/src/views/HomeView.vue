9a93b59edd5adb47e415f347ec689970059a685e
Rossini fonseca <rossinidrum@outlook.com>
2024-02-22 14:17:30 -0300

    core(frontend) inicializa frontend com vue.js

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="font-medium text-red-400">Cybertaza</h1>
+  </main>
+</template>
---
+++ frontend/src/views/admin/LoginView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Login</h1>
+  </main>
+</template>
