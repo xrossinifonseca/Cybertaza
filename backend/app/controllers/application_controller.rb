f9280f4b5e2f030fc229527e40bca715e3762170
Rossini fonseca <rossinidrum@outlook.com>
2024-02-22 12:56:39 -0300

    feat(backend): cria metodo create_admin_token e create_customer_token e set_cookie

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -9,16 +9,30 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, ENV["JWT_KEY"], 'HS256')
   end
 
-  private
   def decode_token(token)
     JWT.decode(token, ENV["JWT_KEY"], true, algorithm: 'HS256')[0]
+
   rescue JWT::DecodeError
     nil
   end
 
   protected
-  def create_token(id)
-    payload = {customer_id:id}
+  def create_customer_token(id)
+    payload = {customer_id:id, rule:"cliente"}
     token = generate_token(payload)
+  end
+
+  def create_admin_token(id)
+    payload = {user_id:id, rule:"admin"}
+    token = generate_token(payload)
+  end
+
+  def set_cookie(token)
+    cookies.signed[:auth_token] = {
+        value: token,
+        httponly: true,
+        secure: true,
+        same_site: :strict
+      }
   end
 end
