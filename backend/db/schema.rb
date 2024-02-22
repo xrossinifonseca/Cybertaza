cd58c4a780c7256242c19d214bf7cf415bdac0a3
Rossini fonseca <rossinidrum@outlook.com>
2024-02-22 12:54:37 -0300

    adiciona tabela "users" e "privileges"

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_035301) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_130132) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "privileges", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -27,4 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_035301) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "privileges_id", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["privileges_id"], name: "index_users_on_privileges_id"
+  end
+
+  add_foreign_key "users", "privileges", column: "privileges_id"
 end
