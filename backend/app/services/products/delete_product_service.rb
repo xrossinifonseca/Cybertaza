51b6cc59611de73e3361298fb147e26d383ba907
Rossini fonseca <rossinidrum@outlook.com>
2024-03-30 18:09:34 -0300

    refactor: change erro message

--- backend/app/services/products/delete_product_service.rb
+++ backend/app/services/products/delete_product_service.rb
@@ -10,7 +10,7 @@ module Products
     end
 
     def delete_product(id)
-      raise "Usuário não tem permissão para criar produtos." if !has_permission?
+      raise "Usuário sem permissão." if !has_permission?
       product = Product.find_by(id: id)
       raise "produto não encontrado." if  product.nil?
       product.destroy
