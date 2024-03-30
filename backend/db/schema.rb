dd917135b7cae4ccfc28533999815a795d4c8945
Rossini fonseca <rossinidrum@outlook.com>
2024-03-30 14:00:27 -0300

    update schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_000356) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_151059) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,13 +71,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_000356) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.integer "privileges_id", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["privileges_id"], name: "index_users_on_privileges_id"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "users", "privileges", column: "privileges_id"
 end
