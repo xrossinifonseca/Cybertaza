f27691e11bf1907245b068ff83b971126a84521c
Rossini fonseca <rossinidrum@outlook.com>
2024-03-30 17:37:32 -0300

    feature: add delete_product_service.rb to handle product deletion

---
+++ backend/app/services/products/delete_product_service.rb
@@ -0,0 +1,25 @@
+
+
+
+module Products
+
+  class DeleteProductService
+
+    def initialize(user)
+      @user = user
+    end
+
+    def delete_product(id)
+      raise "Usuário não tem permissão para criar produtos." if !has_permission?
+      product = Product.find_by(id: id)
+      raise "produto não encontrado." if  product.nil?
+      product.destroy
+    end
+
+
+    private
+    def has_permission?
+    @user.admin?
+    end
+  end
+end
