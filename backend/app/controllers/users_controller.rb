11f92212e021ad421994824788a459e95dad7bae
Rossini fonseca <rossinidrum@outlook.com>
2024-04-30 19:59:21 -0300

    clear methods and add current_user_information method

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,58 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  # before_action :set_user, only: %i[ show edit update destroy ]
 
   def index
     @users = User.all
     render json: {users: users_without_password(@users) }
   end
 
-  def show
+  def current_user_information
+    permissions = Permission::PERMISSION[@current_user.role.to_sym]
+    user = {id:@current_user.id, name: @current_user.name, permissions: permissions}
+    render json: user
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    # respond_to do |format|
-    #   if @user.update(user_params)
-    #     format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @user }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email, :password_digest)
     end
