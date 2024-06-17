efc3c7d2792cdfdfc79903a84b85fed411ad44aa
Rossini fonseca <rossinidrum@outlook.com>
2024-06-17 11:29:43 -0300

    add attribute has_many movements and has_one stock

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,25 +1,34 @@
 class Product < ApplicationRecord
   belongs_to:color
   has_one_attached :image
+  has_many :movements, dependent: :destroy
+  has_one :stock, dependent: :destroy
+  after_create :create_stock
 
   validates :name, presence: { message: "O nome é obrigatório" }
   validates :price, numericality: { greater_than_or_equal_to: 0, message: "preço deve ser maior ou igual a zero" }
   validates :color_id, presence: {message: "Necesário informar cor do produto"}
 
-  # validates :description, presence: { message: "O nome é obrigatório" }
 
-#   def imageUrl
-#     Rails.application.routes.url_helpers.url_for(image) if image.attached?
-#  end
+    def create_stock
+      Stock.create(quantity:0,product_id:id,)
+    end
 
-scope :by_colors, ->(colors) do
-  if colors.is_a?(Array) && !colors.empty?
-    joins(:color).where("colors.name IN (?)", colors)
-  else
-    all
-  end
+
+  scope :by_colors, ->(colors) do
+    if colors.is_a?(Array) && !colors.empty?
+      joins(:color).where("colors.name IN (?)", colors)
+    else
+      all
+    end
+end
 end
 
 
 
-end
+# def create_stock
+#   Stock.transaction do
+#     stock = Stock.create(quantity: 0, product_id: id)
+#     raise ActiveRecord::Rollback if stock.nil? # Rollback if stock creation fails
+#   end
+# end
