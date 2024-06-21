ae7c31f487995050b07ee89c849275cb486f500f
Rossini fonseca <rossinidrum@outlook.com>
2024-06-20 22:02:19 -0300

    update migrations

---
+++ backend/db/migrate/20240618190722_create_movements.rb
@@ -0,0 +1,18 @@
+class CreateMovements < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movements do |t|
+      t.integer :quantity, null: false
+      t.references :product, null: false, foreign_key: true
+      t.string :product_code, null:false
+      t.string :type_movement, null: false
+      t.datetime :date_movement, null: false
+      t.timestamps
+    end
+
+    execute <<-SQL
+    ALTER TABLE movements
+    ADD CONSTRAINT type_movement_check
+    CHECK (type_movement IN ('entrance', 'exit'));
+    SQL
+  end
+end
