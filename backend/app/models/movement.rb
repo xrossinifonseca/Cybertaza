613abded93eb81f9f5f73bcb6312c016f3048e43
Rossini fonseca <rossinidrum@outlook.com>
2024-06-20 22:01:06 -0300

    add new validates

--- backend/app/models/movement.rb
+++ backend/app/models/movement.rb
@@ -2,9 +2,12 @@ class Movement < ApplicationRecord
   belongs_to :product
   after_create :update_stock
 
-  validate :check_stock_quantity, if: :movement_exit?
   validates :quantity, numericality: { greater_than: 0, message: "must be greater than 0" }
+  validates :product_code, presence: {message: "is required"}
+  validates :date_movement, presence: {message: "is required"}
   validates :type_movement, inclusion: { in: ["entrance", "exit"] }
+  validate :check_stock_quantity, if: :movement_exit?
+
 
 
   def movement_exit?
@@ -13,6 +16,9 @@ class Movement < ApplicationRecord
 
 
   def check_stock_quantity
+
+    return errors.add(:product, "Product does not exist") unless product
+
     current_stock_quantity = product.stock.quantity
     if quantity > current_stock_quantity
       errors.add(:quantity, "The exit quantity cannot be greater than the quantity in stock")
