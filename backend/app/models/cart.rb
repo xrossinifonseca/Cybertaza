2ec8f51e971d8b21432eab56e21ecdd64147f9eb
Rossini fonseca <rossinidrum@outlook.com>
2024-08-05 10:50:48 -0300

    refactor: Alias `items` to `cart_items` for improved readability

--- backend/app/models/cart.rb
+++ backend/app/models/cart.rb
@@ -4,13 +4,14 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items
   before_create :check_if_customer_already_has_cart
 
+  alias_method :items, :cart_items
+
   def total
     cart_items.sum {|item| item.total}
   end
 
 
   private
-
   def check_if_customer_already_has_cart
     if customer.present? && Cart.find_by(customer_id: customer.id)
       throw(:abort)
