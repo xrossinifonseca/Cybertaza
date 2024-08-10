a5c66a52091cf83b45866ce69dff04f89ada5485
Rossini fonseca <rossinidrum@outlook.com>
2024-08-09 22:05:37 -0300

    refactor classe

--- backend/app/controllers/api/v1/customers_controller.rb
+++ backend/app/controllers/api/v1/customers_controller.rb
@@ -1,60 +1,20 @@
   class Api::V1::CustomersController < Api::V1::BaseController
 
+    skip_before_action :authenticate_customer, only: [:create]
 
-  include AccessValidator
-
-  def new
-    @customers = Customer.new
-  end
-
-  def index
-    isloggedin = validate_admin(cookies)
-    if isloggedin
-      @customers = Customer.all
-      render json: {customers: customers_without_password(@customers)}
-    end
-  end
-
-  def show
-    isloggedin = validate_admin(cookies)
-      if isloggedin
-      customer  = find_customer(params[:id])
-      if customer
-      return  render json: {customer:customers_without_password(customer)}
-      else
-       render json: {error:"usuário não encontrado"}, status: :unprocessable_entity
-      end
-    end
-  end
 
   def create
     begin
-    Customer.transaction do
-      if Customer.exists?(email:customer_params[:email])
-       return render json: {error:"usuário ja cadastrado"}, status: :unprocessable_entity
-      end
-
-      @customers = Customer.create(customer_params)
-
-      if @customers.save
-        token = create_token(@customers.id)
-      return  render json: {message:"usuário cadastrado com sucesso!",user:@customers,token:token}, status: :ok
-    end
-      render json: { error: @customers.errors.messages }, status: :unprocessable_entity
-    end
-
+    new_customer = Customer.create!(customer_params)
+    create_customer_token(new_customer.id)
+    render json: {message:"customer registered successfully", customer: CustomerSerializer.new(new_customer)}, status: :created
     rescue ActiveRecord::RecordInvalid => e
     render json: { error: e.message }, status: :unprocessable_entity
     end
   end
 
-  def profile
-    isloggedin = validate(cookies)
-    if isloggedin
-      id = isloggedin['customer_id']
-      customer = find_customer(id)
-      render json: {customer:customers_without_password(customer)}
-    end
+  def customer_data
+    render json: @current_customer, serializer: CustomerSerializer
   end
 
   private
@@ -62,16 +22,5 @@
     params.permit(:name,:email,:password)
   end
 
-  def customers_without_password(customer)
-     customer.as_json(except: :password_digest)
-  end
-
-  def find_customer(id)
-    if Customer.exists?(id:id)
-      @customers = Customer.find(id)
-      return   customers_without_password(@customers)
-      end
-      nil
-  end
 
 end
