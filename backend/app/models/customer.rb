1aff4c7ed07003fbeb1b34b9da1b897df9271069
Rossini fonseca <rossinidrum@outlook.com>
2024-08-09 22:07:01 -0300

    update message validates

--- backend/app/models/customer.rb
+++ backend/app/models/customer.rb
@@ -2,9 +2,9 @@ class Customer < ApplicationRecord
   has_one :cart
   has_secure_password
 
-  validates :name, presence: { message: "O nome é obrigatório" }
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP,message:"Email inválido" }
-  validates :password, presence: { message: "A senha é obrigatória" }, length: { minimum: 6, message: "A senha deve ter pelo menos 6 caracteres" }
+  validates :name, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence:true, length: { minimum: 6 }
 
 
 end
